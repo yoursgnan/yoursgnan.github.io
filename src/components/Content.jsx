a0b9c443a9e3e4e4983f7ce10b47bd3ff2a8e3ce
Gnana Prakash <purpleowlcodes@gmail.com>
2023-08-27 13:32:45 +0530

    contact profiles updated
    contact profiles updated

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,3 +1,6 @@
+import linkedin from '../assets/linkedin.png'
+import insta from '../assets/instagram.png'
+
 const Content = () => {
     return (
         <div className="main-content">
@@ -10,6 +13,14 @@ const Content = () => {
                 I'm a Software developer, Me as technology enthusiast, always looking for the opportunities
                 which constantly drives me and encourages me to learn something new in the everyday trends.
                 </p>
+                <div className="share-container">
+                    <a href="https://www.linkedin.com/in/iamgnanaprakash">
+                        <img src={linkedin} width={30} height={30}/>
+                    </a>
+                    <a href="https://www.instagram.com/yoursgnan">
+                        <img src={insta} width={30} height={30}/>
+                    </a>
+                </div>
             </div>
         </div>
     )
